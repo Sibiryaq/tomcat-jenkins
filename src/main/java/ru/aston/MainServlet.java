f4225c3e9227c6a2a4b4976bf839a332ef786bf4
Sibiryaq <alexander.tolst1kov@yandex.ru>
2024-03-17 22:43:32 +0700

    !!

--- src/main/java/ru/aston/MainServlet.java
+++ src/main/java/ru/aston/MainServlet.java
@@ -15,7 +15,7 @@ public class MainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter pw = resp.getWriter();
-        pw.write("Hello everybody! Put your hand up!!!");
+        pw.write("Hello everybody! Put your hand up!");
         pw.close();
     }
 }
